42b9c39ef3fb1ea8b5b0db832bfc1eebb96756ce
Briarwoodsz <johnjosephhoy@gmail.com>
2020-01-10 14:41:22 -0700

    edited client file api routes and page files
    page files now need the actual logic, and the api route can connect to the creating and deleting pages now.

---
+++ client/src/utils/API.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+export default {
+
+},
+getBooks: function() {
+     return axios.get("/api/books");
+},
+getBook: function(id) {
+     return axios.get(`/api/books${id}`);
+},
+saveBook: function(savedBooks) {
+     return axios.post(`/api/books`, savedBooks);
+},
+deleteBook: function(id) {
+     return axios.delete(`/api/books/${id}`)
+}
+}
